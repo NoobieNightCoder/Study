598485db389d27edaaf88fd469f1149ac2c5305d
Noobie <dmaslo07@mail.ru>
2026-04-24 12:05:48 +0300

    Delete Aboba directory

--- OP/1 Year 2 Sem/Aboba/ui/app_controller.cpp
+++
@@ -1,8 +0,0 @@
-#include "app_controller.h"
-#include "../logic/logic.h"
-
-QString AppController::handleButtonClick()
-{
-    const char* text = get_new_text();
-    return QString::fromUtf8(text);
-}

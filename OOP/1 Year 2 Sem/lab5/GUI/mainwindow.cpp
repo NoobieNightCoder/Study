1b119132fa90e1bea0cfaa8031e596785f6dde65
NoobieNightCoder <dmaslo07@mail.ru>
2026-06-06 21:37:52 +0300

    Add lab5 OOP

---
+++ OOP/1 Year 2 Sem/lab5/GUI/mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <string>
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    connect(ui->elevatorCall1Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->elevatorCall2Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->elevatorCall3Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->elevatorCall4Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->elevatorCall5Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+
+    connect(ui->homeCall1Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->homeCall2Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->homeCall3Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->homeCall4Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+    connect(ui->homeCall5Button, &QPushButton::clicked, this, &MainWindow::floorButtonClicked);
+
+    ui->setupUi(this);
+}
+
+void MainWindow::floorButtonClicked() {
+    QPushButton *button = qobject_cast<QPushButton*>(sender());
+    if (button) {
+        QString text = button->text();
+        ui->currentFloorLabel->setText(text);
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
